c7601cc81195111aa23163e8653c983e4a4b1f03
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-12-05 07:12:56 +0000

    5 part 1

---
+++ 5/part1.py
@@ -0,0 +1,18 @@
+with open("input") as f:
+    a, b = f.read().split("\n\n")
+
+stacks = {i: [] for i in range(1, 10)}
+for row in a.split("\n")[-2::-1]:
+    for i, j in enumerate(range(1, len(row), 4)):
+        if row[j] != " ":
+            stacks[i+1].append(row[j])
+
+for line in b.split("\n"):
+    if line != "":
+        n = int(line.split("move ")[1].split(" from")[0])
+        start = int(line.split("from ")[1].split(" to")[0])
+        end = int(line.split("to ")[1])
+        stacks[end] += stacks[start][-n:][::-1]
+        stacks[start] = stacks[start][:-n]
+
+print("".join(i[-1] for i in stacks.values()))
