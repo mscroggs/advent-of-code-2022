691b0deac9d726345cf2a5aa4783bb2b7f97745c
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2022-12-14 10:56:17 +0000

    14.2

---
+++ 14/part2.py
@@ -0,0 +1,44 @@
+cavern = {}
+
+def get_contents(x, y):
+    if (x, y) in cavern:
+        return cavern[(x, y)]
+    return None
+
+max_y = 0
+
+with open("input") as f:
+    for line in f:
+        ls = line.strip().split(" -> ")
+        for start, end in zip(ls[:-1], ls[1:]):
+            start = [int(i) for i in start.split(",")]
+            end = [int(i) for i in end.split(",")]
+
+            for i in range(min(start[0], end[0]), max(start[0], end[0]) + 1):
+                for j in range(min(start[1], end[1]), max(start[1], end[1]) + 1):
+                    cavern[(i, j)] = "ROCK"
+
+            max_y = max(start[1], end[1], max_y)
+
+def drop_sand():
+    sandpos = (500, 0)
+
+    while True:
+        if sandpos[1] == max_y + 1:
+            return sandpos
+        if get_contents(sandpos[0], sandpos[1] + 1) is None:
+            sandpos = (sandpos[0], sandpos[1] + 1)
+        elif get_contents(sandpos[0] - 1, sandpos[1] + 1) is None:
+            sandpos = (sandpos[0] - 1, sandpos[1] + 1)
+        elif get_contents(sandpos[0] + 1, sandpos[1] + 1) is None:
+            sandpos = (sandpos[0] + 1, sandpos[1] + 1)
+        else:
+            return sandpos
+n = 0
+while True:
+    n += 1
+    sandpos = drop_sand()
+    cavern[sandpos] = "SAND"
+    if sandpos == (500, 0):
+        break
+print(n)
